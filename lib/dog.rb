067a974851f5d869b4c25091b7245a6d6467add7
Matthew C <m23.cullen@gmail.com>
2019-10-16 19:29:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,6 +47,9 @@ class Dog
     dog = self.new(id: row[0], name: row[1], breed: row[2])
   end 
   
+  def self.find_by_id
+  
+  end 
   def self.find_by_name(name)
     sql = <<-SQL
         SELECT*
