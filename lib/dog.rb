cfe04b6435f0d6a3bfa5b1fd7af3d6fefa1b26eb
Matthew C <m23.cullen@gmail.com>
2019-10-16 19:49:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,6 +70,19 @@ class Dog
   end 
   
   def self.find_or_create_by
-    
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ? AND breed = ?
+      SQL
+
+
+      dog = DB[:conn].execute(sql, name, breed).first
+
+      if dog
+        new_dog = self.new_from_db(dog)
+      else
+        new_dog = self.create({:name => name, :breed => breed})
+      end
+      new_dog
   end 
 end 
