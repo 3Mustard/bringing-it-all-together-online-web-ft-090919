25c1cc7d8b0d4ebe1250adcdaf3226b0cb20c374
Matthew C <m23.cullen@gmail.com>
2019-10-16 19:05:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,25 @@ class Dog
     @name = name 
     @breed = breed 
   end 
+  
+  def self.create_table
+    sql = <<-SQL 
+        CREATE TABLE IF NOT EXISTS dogs 
+        (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)
+      SQL
+    DB[:conn].execute(sql)
+  end 
+  
+  def self.drop_table 
+    DB[:conn].execute("DROP TABLE dogs")
+  end 
+  
+  def save
+    sql = <<-SQL 
+        
+      SQL
+  end 
+  
+  def self.create 
+  end 
 end 
