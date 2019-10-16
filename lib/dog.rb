1b1312d9b70262c52a1cef1e79fbbfd48af7b112
Matthew C <m23.cullen@gmail.com>
2019-10-16 19:28:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,31 +43,19 @@ class Dog
     dog
   end 
   
-  def self.new_from_db(array)
-    dog = self.new()
-    
+  def self.new_from_db(row)
+    dog = self.new(id: row[0], name: row[1], breed: row[2])
   end 
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  def self.find_by_name(name)
+    sql = <<-SQL
+        SELECT*
+        FROM dogs
+        WHERE name = ?
+        LIMIT 1 
+      SQL
+    db[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end 
+  end 
 end 
